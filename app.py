e462a059109f7f457c508bc8e661dabcef66cd2d
tarabakz25 <aikikizuki@gmail.com>
2024-08-22 11:38:30 +0900

    新しい仮想環境の作成

--- app.py
+++ app.py
@@ -25,8 +25,6 @@ app = Flask(__name__)
 configuration = Configuration(access_token=config.CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(config.CHANNEL_SECRET)
 
-
-
 @app.route('/callback', methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -41,12 +39,13 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         linebot_api = MessagingApi(api_client)
+        reply_text = event.message.text  # ユーザーから送られたテキストを取得
         linebot_api.reply_message_with_http_info(
             ReplyMessageRequest(
-                reply_token = event.reply.token,
-                messages=[TextMessage(text=event.message.text)]
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply_text)]  # 取得したテキストをそのまま返信
             )
         )
-        
+
 if __name__ == "__main__":
     app.run(port=8080)
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 CHANNEL_ACCESS_TOKEN="IgIIuAg3RHLqeK/bJtmn+EG0wPqNpJT3oK0yGOFBJRmgp2xxXextCBTY3Q0hM9M5uLP6mfJ883aSdu7d3MUyGII6pDHmaVGqH4yjBdCIUtQ0ejectrrlwlwyZ2v7zowAbUqGg1LP5IpaSKWt0pXrmQdB04t89/1O/w1cDnyilFU="
 CHANNEL_SECRET="3e4c5c791ac329fc3d264e53c0b80b12"
 
-# ngrokのhttpリンク https://funky-intensely-stingray.ngrok-free.app 8080
+# ngrokのhttpリンク ngrok http https://funky-intensely-stingray.ngrok-free.app 8080
 #
 #
 #
